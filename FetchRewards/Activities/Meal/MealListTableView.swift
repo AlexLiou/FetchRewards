d3215e3762a4640ee65885b47b30d47b0aeaa74b
alexliou <alexliou0@gmail.com>
2022-07-04 13:20:18 -0400

    dsafd

---
+++ FetchRewards/Activities/Meal/MealListTableView.swift
@@ -0,0 +1,21 @@
+//
+//  MealListTableView.swift
+//  FetchRewards
+//
+//  Created by Alex Liou on 7/4/22.
+//
+
+import UIKit
+
+class MealListTableView: UITableView {
+
+    init() {
+        super.init(frame: .zero)
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
